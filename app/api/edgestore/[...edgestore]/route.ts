69343ffec362d5abae321c05600ba06451c14949
Rob Bettison <rbettison>
2023-11-22 10:55:06 +0100

    Edge store configuration for example

---
+++ app/api/edgestore/[...edgestore]/route.ts
@@ -0,0 +1,19 @@
+import { initEdgeStore } from '@edgestore/server';
+import { createEdgeStoreNextHandler } from '@edgestore/server/adapters/next/app';
+
+const es = initEdgeStore.create();
+
+const edgeStoreRouter = es.router({
+  publicFiles: es.imageBucket({
+    maxSize: 1024 * 1024 * 10, // 10MB
+    accept: ['image/jpeg', 'image/png']
+  }),
+});
+
+const handler = createEdgeStoreNextHandler({
+  router: edgeStoreRouter,
+});
+
+export { handler as GET, handler as POST };
+
+export type EdgeStoreRouter = typeof edgeStoreRouter;
